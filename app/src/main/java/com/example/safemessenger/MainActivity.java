a73aa3b00c603dda93341f4ea0dc2380636377c1
Aetaugan <regis@et.esiea.fr>
2020-01-06 00:01:55 +0200

    Try

--- app/src/main/java/com/example/safemessenger/MainActivity.java
+++ app/src/main/java/com/example/safemessenger/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Try
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
