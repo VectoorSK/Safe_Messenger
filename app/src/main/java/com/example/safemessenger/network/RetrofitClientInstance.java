91be38fa0c62202d285191baad6279d2870d2aa8
Aetaugan <regis@et.esiea.fr>
2020-01-06 12:40:10 +0200

    Retrofit Configured for MongoDB

---
+++ app/src/main/java/com/example/safemessenger/network/RetrofitClientInstance.java
@@ -0,0 +1,21 @@
+package com.example.safemessenger.network;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+public class RetrofitClientInstance {
+
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "mongodb+srv://Aetaugan:Wo5a7zg#Kk&5MC4yhA%sP&dE7b$mtX@smdb-8uo3d.azure.mongodb.net/test?retryWrites=true&w=majority";
+
+    public static Retrofit getRetrofitInstance(){
+        if (retrofit == null){
+            retrofit = new retrofit2.Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
